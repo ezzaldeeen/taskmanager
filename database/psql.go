060a4a1780f69862c52dca06c652e5a608315f16
Ezzaldeen Mousa <ezzaldeeenmousa@gmail.com>
2023-05-11 18:31:47 +0300

    add db driver

---
+++ database/psql.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DBDriver struct {
+	user     string
+	name     string
+	pwd      string
+	port     string
+	sslMode  string
+	timeZone string
+}
+
+func NewDBDriver(user, name, pwd, port string) *DBDriver {
+	return &DBDriver{
+		user: user,
+		name: name,
+		pwd:  pwd,
+		port: port,
+	}
+}
+
+func (d DBDriver) GetConnection() (*gorm.DB, error) {
+	// setting database configuration
+	dns := fmt.Sprintf(
+		"user=%s password=%s dbname=%s port=%s",
+		d.user, d.pwd, d.name, d.port)
+	cfg := postgres.Config{
+		DSN:                  dns,
+		PreferSimpleProtocol: true,
+	}
+	// getting new db connection
+	db, err := gorm.Open(postgres.New(cfg))
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
