a295720aabf56bf48213d0656adab0062177edc3
Ezzaldeen Mousa <ezzaldeeenmousa@gmail.com>
2023-05-14 19:21:28 +0300

    clean up

--- controllers/health/v1.go
+++ controllers/health/v1.go
@@ -3,21 +3,32 @@ package health
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"taskmanager/database"
 )
 
-// RegisterRouteGrp registers the route group for tasks under the provided echo.Group.
+type Controller struct {
+	g *echo.Group
+}
+
+func NewController(g *echo.Group, driver *database.DBDriver) *Controller {
+	return &Controller{
+		g: g,
+	}
+}
+
+// Register registers the route group for tasks under the provided echo.Group.
 // It creates a subgroup "/v1/health" under the given group and registers
 // the relevant HTTP handlers.
 // The following routes are registered:
-// - GET: the health status of the service
-func RegisterRouteGrp(g *echo.Group) {
-	taskGrp := g.Group("/v1/health")
-	taskGrp.GET("", get)
+// - GET: Retrieve health of the service
+func (c Controller) Register() {
+	grp := c.g.Group("/v1/health")
+	grp.GET("", c.get)
 }
 
-// get is an HTTP handler function that check the health of the service.
-func get(c echo.Context) error {
+// getAll is an HTTP handler function that the health of the service.
+func (c Controller) get(ec echo.Context) error {
 	// todo: implement health checking
 	// todo: check DB, Redis connection status
-	return c.JSON(http.StatusOK, "OK")
+	return ec.JSON(http.StatusOK, "OK")
 }
--- controllers/task/v1.go
+++ controllers/task/v1.go
@@ -1,11 +1,31 @@
 package task
 
 import (
+	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"taskmanager/database"
+	"taskmanager/models/task"
+	"taskmanager/repositories/task"
+	"taskmanager/services/task"
+	"taskmanager/utils"
 )
 
-// RegisterRouteGrp registers the route group for tasks under the provided echo.Group.
+type Controller struct {
+	g       *echo.Group
+	service *tasksvc.Service
+}
+
+func NewController(g *echo.Group, driver *database.DBDriver) *Controller {
+	repo := taskrepo.NewRepo(driver)
+	svc := tasksvc.NewService(repo)
+	return &Controller{
+		g:       g,
+		service: svc,
+	}
+}
+
+// Register registers the route group for tasks under the provided echo.Group.
 // It creates a subgroup "/v1/tasks" under the given group and registers
 // the relevant HTTP handlers.
 // The following routes are registered:
@@ -14,40 +34,41 @@ import (
 // - POST: Create a new task
 // - PUT: Update a task by its ID todo (Not implemented)
 // - DELETE: Delete a task by its ID todo (Not implemented)
-func RegisterRouteGrp(g *echo.Group) {
-	taskGrp := g.Group("/v1/tasks")
-	taskGrp.GET("", getAll)
-	taskGrp.GET("/:id", getById)
-	taskGrp.POST("", create)
-	taskGrp.PUT("/:id", nil)
-	taskGrp.DELETE("/:id", nil)
+func (c Controller) Register() {
+	grp := c.g.Group("/v1/tasks")
+	grp.GET("", c.getAll)
+	grp.GET("/:id", c.getById)
+	grp.POST("", c.create)
+	grp.PUT("/:id", nil)
+	grp.DELETE("/:id", nil)
 }
 
 // getAll is an HTTP handler function that retrieves all tasks.
-func getAll(c echo.Context) error {
-	return c.JSON(http.StatusOK, "TASKS")
+func (c Controller) getAll(ec echo.Context) error {
+	idos, err := c.service.GetAllTask()
+	if err != nil {
+		return ec.JSON(http.StatusNotFound, err)
+	}
+	dtos := utils.ConvertIDOStoDTOS(idos)
+	return ec.JSON(http.StatusCreated, dtos)
 }
 
 // getById is an HTTP handler function that retrieves a task by its ID.
-func getById(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Task By Id")
+func (c Controller) getById(ec echo.Context) error {
+	return ec.JSON(http.StatusOK, "Task By Id")
 }
 
 // create is an HTTP handler function that handles the creation of a new task.
-func create(c echo.Context) error {
-	//dto := task.DTO{}
-	//err := json.NewDecoder(c.Request().Body).Decode(&dto)
-	//if err != nil {
-	//	return c.JSON(http.StatusBadRequest, err)
-	//}
-	//
-	//task3.NewRepo()
-	//svc := task2.NewService()
-	//err = svc.CreateTask(dto.ToIDO())
-	//if err != nil {
-	//	return c.JSON(http.StatusBadRequest, err)
-	//}
-	//return c.JSON(http.StatusCreated, nil)
-
-	return nil
+func (c Controller) create(ec echo.Context) error {
+	payload := new(task.DTO)
+	// todo: add validation
+	err := json.NewDecoder(ec.Request().Body).Decode(&payload)
+	if err != nil {
+		return ec.JSON(http.StatusBadRequest, err)
+	}
+	err = c.service.CreateTask(payload.ToIDO())
+	if err != nil {
+		return ec.JSON(http.StatusBadRequest, err)
+	}
+	return ec.JSON(http.StatusCreated, payload)
 }
